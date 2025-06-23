e83421c623b61dbd8ebf3e6b46a925c08c85de18
bella234428 <ibseali258@gmail.com>
2025-06-23 10:07:35 +0300

    add javascript code

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+// Provided variables
+const num1 = 31;
+const num2 = 2;
+const num3 = 20;
+const num4 = 8;
+
+// Solutions
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 100) + 1;
+const mod = num3 % num4;
+const max = Math.max(1, 5, 10, 20);
